cd933f6d05c68c77a5026ad11e422c02a9ea7198
Ahtuz <33178140+Ahtuz@users.noreply.github.com>
2018-12-14 21:48:23 +0700

    add score sorting update

--- save-file-score/save-file-score.cpp
+++ save-file-score/save-file-score.cpp
@@ -1,181 +1,117 @@
 #include<stdio.h>
 #include<string.h>
 
-struct soal09{
-	char nama[20];
-	int avgskor;
-	int play;
-	int skor;
-};
+	int choice, index, skortemp, ctr, min;
+	char namatemp[20];
+	FILE*p=fopen("soal09.txt","r");
 
+	struct player{
+		char name[20];
+		int avgscore;
+		int numofplay;
+	}player[100], temp;
+	
+	void sort(){
+		for(int i=0;i<index;i++){
+			for(int j=0;j<index-1;j++){
+				if(player[j].avgscore<player[j+1].avgscore){
+					temp=player[j];
+					player[j]=player[j+1];
+					player[j+1]=temp;
+				}
+			}
+		}
+	}
+
+	void selsort(){
+		for(int i=0;i<index;i++){
+			min=i;
+			for(int j=i;j<index;j++){
+				if(player[min].avgscore>player[j].avgscore){
+					min=j;
+				}
+			}
+			temp=player[i];
+			player[i]=player[min];
+			player[min]=temp;
+		}
+	}
 
 int main(){
-	int pilih, index = 0;
-	struct soal09 filee[20];
-	char namatemp[30];
-	int count=0;
-	char deletenama[20];
-	int deletetemp;
-	char tambahNamaTemp[20];
-	int ctr=0;
-
-
-	FILE *p;
-	p = fopen("soal09.txt", "r");
-	while(fscanf(p, "%s %d %d\n", filee[index].nama, &filee[index].avgskor, &filee[index].play) != EOF) index++;
-	fclose(p);
-
+	while(fscanf(p, "%s %d %d", player[index].name, &player[index].avgscore, &player[index].numofplay)!=EOF) index++;
 	do{
-		for(int i=0;i<10;i++){
-				printf("\n");
-		}
-		printf("Score Record DotA Player\n========================\n1.View Record\n2.Add Player Record\n3.Add New Player\n4.Delete Player\n5.Save and Exit");
-	
+		selsort();
+		printf("\n\n\n\n\n\n\n\n\n\n");
+		printf("Score Record DotA Player\n========================\n1.View Record\n2.Add Player Record\n3.Add New Player\n4.Delete Player\n5.Save and Exit\n\n");
+
 		do{
-			printf("\nInput your choice[1..5]:");
-			scanf("%d", &pilih); fflush stdin;
-		}while(pilih<1 || pilih>5);
+			printf("Input your choice[1..5]:");
+			scanf("%d", &choice); fflush stdin;
+		}while(choice < 0 || choice > 5);
 
-		switch(pilih){
+		switch(choice){
 		case 1:
-			for(int i=0;i<30;i++){
-				printf("\n");
-			}
-
-			printf("Player Name     | Average Score   | Number of Playing\n=======================================================\n");
+			printf("\n\n\n\n\n\n\n\n\n\n");
+			printf(" Player Name     | Average Score   | Number of Playing\n=======================================================\n");
 			for(int i=0;i<index;i++){
-				printf("%-15s | %-15d | %-15d\n", filee[i].nama, filee[i].avgskor, filee[i].play);
+				printf(" %-15s | %-15d | %-15d\n", player[i].name, player[i].avgscore, player[i].numofplay);
 			}
-
-			printf("\n\nPress Enter to Continue...");
 
 			getchar();
-
 			break;
-
 		case 2:
-			for(int i=0;i<30;i++){
-				printf("\n");
-			}
-			count=0;
-
+			printf("\n\n\n\n\n\n\n\n\n\n");
 			printf("Input player name[1..10]:");
 			scanf("%s", namatemp); fflush stdin;
 
-			for(int i = 0;i<index;i++){
-				if(strcmpi(filee[i].nama, namatemp)==0){
-					do{
-						printf("Input player score[0..100]:");
-						scanf("%d", &filee[i].skor); fflush stdin;
-					}while(filee[i].skor > 100 || filee[i].skor < 0);
-					filee[i].play++;
-					filee[i].avgskor = ((filee[i].avgskor*(filee[i].play-1)) + filee[i].skor) / filee[i].play;
-					printf("Data successfully updated!");
-				}
-				else count++;
-			}
-
-			if(count==index){
-				printf("Name of %s not found!", namatemp);
-				count=0;
-			}
-
-			p=fopen("soal09.txt","w");
 			for(int i=0;i<index;i++){
-				fprintf(p, "%s %d %d\n", filee[i].nama, filee[i].avgskor, filee[i].play);
+				if(strcmpi(namatemp, player[i].name)==0){
+					printf("Input player score[0..100]:");
+					scanf("%d", &skortemp); fflush stdin;
+					player[i].avgscore=((player[i].avgscore*player[i].numofplay)+skortemp)/(player[i].numofplay+1);
+					player[i].numofplay+=1;
+				}
 			}
-			fclose(p);
-
-			printf("\n\nPress Enter to Continue...");
 
 			getchar();
-
 			break;
-
 		case 3:
-			for(int i=0;i<30;i++){
-				printf("\n");
-			}
-
-			ctr=0;
-
-			do{
-				printf("Input Player's name:");
-				scanf("%s", tambahNamaTemp); fflush stdin;
-			}while(strlen(tambahNamaTemp)>10);
-
-			for(int i=0;i<index;i++){
-				if(strcmpi(tambahNamaTemp,filee[i].nama)==0){
-					ctr=1;
-				}
-			}
-
-			if(ctr==0){
-				strcpy(filee[index].nama,tambahNamaTemp);
-				filee[index].avgskor=0;
-				filee[index].play=0;
-				printf("%s successfully added!", filee[index].nama);
-				index++;
-			}
-
-			if(ctr==1){
-				printf("Player name already exist.");
-			}
-
-			printf("\n\nPress Enter to Continue...");
-
+			printf("\n\n\n\n\n\n\n\n\n\n");
+			printf("Input player name[1..10]:");
+			scanf("%s", namatemp); fflush stdin;
 			
+			strcpy(player[index].name, namatemp);
+			player[index].avgscore=0;
+			player[index].numofplay=0;
+
+			index++;
 
 			getchar();
-
 			break;
-
 		case 4:
-			for(int i=0;i<30;i++){
-				printf("\n");
-			}
+			printf("\n\n\n\n\n\n\n\n\n\n");
+			printf("Input player name[1..10]:");
+			scanf("%s", namatemp); fflush stdin;
 
-			count = 0;
-
-			printf("Input player name:");
-			scanf("%s", deletenama); fflush stdin;
-
-			for(int i=0;i<index;i++){
-				if(strcmpi(filee[i].nama, deletenama)==0){
-						deletetemp = i;
-						for(int i = deletetemp; i<index;i++){
-							filee[i]=filee[i+1];
-						}
-						index--;
-						count=1;
-						printf("%s successfully deleted.", deletenama);
+			for(int i =0;i<index;i++){
+				if(strcmp(player[i].name, namatemp)==0){
+					for(int j=i;j<index;j++){
+						player[j]=player[j+1];
+					}
+					index--;
 				}
 			}
 
-			if(count==0){
-				printf("Name not found!");
-			}
-
-			printf("\n\nPress Enter to Continue...");
-
 			getchar();
-
 			break;
-
 		}
+	}while(choice!=5);
 
-	}while(pilih!=5);
-
-	p = fopen("soal09.txt","w");
-
+	FILE*p=fopen("soal09.txt","w");
 	for(int i=0;i<index;i++){
-		fprintf(p, "%s %d %d\n", filee[i].nama, filee[i].avgskor, filee[i].play);
+		fprintf(p, "%s %d %d\n", player[i].name, player[i].avgscore, player[i].numofplay);
 	}
-
 	fclose(p);
-
-	printf("\n\nData successfully saved to \"soal09.txt\".\nPress Enter to exit the program.");
-
 	getchar();
 	return 0;
 }
